47e14b6a44494f1997c43c3be8c7658028d210b4
Martin Panzer <postremus1996@googlemail.com>
2018-05-04 23:01:02 +0200

    Add ci with travis deploy to maven central build docker images, and deploy them to docker hub

---
+++ cd/before-install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+cp cd/.travis.settings.xml $HOME/.m2/settings.xml
+#if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+    openssl aes-256-cbc -K $encrypted_2789e35462ef_key -iv $encrypted_2789e35462ef_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
+    gpg --fast-import cd/codesigning.asc
+#fi
