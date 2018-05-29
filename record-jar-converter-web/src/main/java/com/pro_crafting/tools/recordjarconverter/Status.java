30cbda67b5c35717e117ba91ba2e3310a9475bb2
Martin Panzer <postremus1996@googlemail.com>
2018-05-29 16:05:01 +0200

    Add health endpoint for now, there are no other information needed closes #5
    remove jaxrs from core, not needed

---
+++ record-jar-converter-web/src/main/java/com/pro_crafting/tools/recordjarconverter/Status.java
@@ -0,0 +1,16 @@
+package com.pro_crafting.tools.recordjarconverter;
+
+import org.eclipse.microprofile.health.Health;
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@Health
+@ApplicationScoped
+public class Status implements HealthCheck {
+    @Override
+    public HealthCheckResponse call() {
+        return HealthCheckResponse.named("record-jar-converter-web").up().build();
+    }
+}
