d49c6c100bfbb52199296bf8b92333a94479fef1
Martin Panzer <postremus1996@googlemail.com>
2018-07-08 13:49:51 +0200

    don't even start ws on build stage only try to run it for the web artifact

--- cd/build.sh
+++ cd/build.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-mvn install -DskipITs -P docker
+mvn install -DskipITs -P docker,!ws-it
--- cd/docker-it.sh
+++ cd/docker-it.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+cd ./record-jar-converter-web/
 mvn -Dmaven.javadoc.skip=true -DskipTests verify -P docker-it,!ws-it
