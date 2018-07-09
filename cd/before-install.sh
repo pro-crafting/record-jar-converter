983a1da2e0e983a865d421d80d0c118dce213789
Martin Panzer <postremus1996@googlemail.com>
2018-07-09 20:40:23 +0200

    try to up - and download the current build context to tm.pro-crafting.com

--- cd/before-install.sh
+++ cd/before-install.sh
@@ -4,3 +4,9 @@ cp cd/.travis.settings.xml $HOME/.m2/settings.xml
     openssl aes-256-cbc -K $encrypted_2789e35462ef_key -iv $encrypted_2789e35462ef_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
     gpg --fast-import cd/codesigning.asc
 #fi
+
+#Add public key for data@tm.pro-crafting.com
+openssl aes-256-cbc -K $encrypted_2789e35462ef_key -iv $encrypted_2789e35462ef_iv -in cd/id_rsa_data.pub.enc -out cd/id_rsa_data.pub -d
+copy cd/id_rsa_data.pub ~/.id_rsa_data.pub
+chmod 0644 ~/.id_rsa_data.pub
+scp data@tm.pro-crafting.com/home/data/$TRAVIS_BUILD_NUMBER/ ./
---
+++ cd/upload-build-context.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+scp ./ data@tm.pro-crafting.com/home/data/$TRAVIS_BUILD_NUMBER/
