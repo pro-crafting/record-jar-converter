ec21e9dd72b42c747259731fc90de818567d7a4d
Martin Panzer <postremus1996@googlemail.com>
2018-07-09 21:31:58 +0200

    fix path to secret-files.tar

--- cd/before-install.sh
+++ cd/before-install.sh
@@ -2,7 +2,7 @@
 cp cd/.travis.settings.xml $HOME/.m2/settings.xml
 openssl aes-256-cbc -K $encrypted_2789e35462ef_key -iv $encrypted_2789e35462ef_iv -in cd/secret-files.tar.enc -out cd/secret-files.tar -d
 cd cd/
-tar xvf cd/secret-files.tar
+tar xvf secret-files.tar
 cd ..
 
 #if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
