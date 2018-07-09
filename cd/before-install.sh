b8055d89d270f9baeaa2ab258aa181ee5ceb4b02
Martin Panzer <postremus1996@googlemail.com>
2018-07-09 21:20:24 +0200

    Only one file can be encrypted

--- cd/before-install.sh
+++ cd/before-install.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 cp cd/.travis.settings.xml $HOME/.m2/settings.xml
+openssl aes-256-cbc -K $encrypted_2789e35462ef_key -iv $encrypted_2789e35462ef_iv -in cd/secret-files.tar.enc -out cd/secret-files.tar -d
+cd cd/
+tar xvf cd/secret-files.tar
+cd ..
+
 #if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_2789e35462ef_key -iv $encrypted_2789e35462ef_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
     gpg --fast-import cd/codesigning.asc
 #fi
 
 #Add public key for data@tm.pro-crafting.com
-openssl aes-256-cbc -K $encrypted_2789e35462ef_key -iv $encrypted_2789e35462ef_iv -in cd/id_rsa_data.pub.enc -out cd/id_rsa_data.pub -d
-cp cd/id_rsa_data.pub ~/.id_rsa_data.pub
-chmod 0644 ~/.id_rsa_data.pub
+cp cd/id_rsa_data.pub ~/.ssh/.id_rsa_data.pub
+chmod 0644 ~/.ssh/.id_rsa_data.pub
 scp data@tm.pro-crafting.com/home/data/$TRAVIS_BUILD_NUMBER/ ./
