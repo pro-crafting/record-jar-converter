01394e92f0b722eac3e989e90dab7bebd29cae59
Martin Panzer <postremus1996@googlemail.com>
2018-07-13 21:20:25 +0200

    try to just put the target files into the build cache

--- cd/before-install.sh
+++ cd/before-install.sh
@@ -1,16 +1,11 @@
 #!/usr/bin/env bash
 cp cd/.travis.settings.xml $HOME/.m2/settings.xml
 openssl aes-256-cbc -K $encrypted_2789e35462ef_key -iv $encrypted_2789e35462ef_iv -in cd/secret-files.tar.enc -out cd/secret-files.tar -d
-cd cd/
-tar xvf secret-files.tar
-cd ..
-
+mkdir cache
 #if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     gpg --fast-import cd/codesigning.asc
 #fi
 
-#Add public key for data@tm.pro-crafting.com
-cp cd/id_rsa_data.pub ~/.ssh/id_rsa.pub
-chmod 0644 ~/.ssh/id_rsa.pub
-ssh-keyscan -t rsa -H tm.pro-crafting.com >> ~/.ssh/known_hosts
-scp data@tm.pro-crafting.com:/home/data/$TRAVIS_BUILD_NUMBER/ ./
+cp -r cache/record-jar-converter-rest-api/target record-jar-converter-rest-api/target
+cp -r cache/record-jar-converter-service/target record-jar-converter-service/target
+cp -r cache/record-jar-converter-web/target record-jar-converter-web/target
--- cd/upload-build-context.sh
+++ cd/upload-build-context.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-scp ./ data@tm.pro-crafting.com:/home/data/$TRAVIS_BUILD_NUMBER/
+cp -r record-jar-converter-rest-api/target cache/record-jar-converter-rest-api/target
+cp -r record-jar-converter-service/target cache/record-jar-converter-service/target
+cp -r record-jar-converter-web/target cache/record-jar-converter-web/target
