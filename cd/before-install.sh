27b9c17e226da2a42ae442192452c1a62f24aab6
Martin Panzer <postremus1996@googlemail.com>
2018-07-13 20:40:34 +0200

    rename shit

--- cd/before-install.sh
+++ cd/before-install.sh
@@ -10,7 +10,7 @@ cd ..
 #fi
 
 #Add public key for data@tm.pro-crafting.com
-cp cd/id_rsa_data.pub ~/.ssh/id_rsa_data.pub
-chmod 0644 ~/.ssh/id_rsa_data.pub
+cp cd/id_rsa_data.pub ~/.ssh/id_rsa.pub
+chmod 0644 ~/.ssh/id_rsa.pub
 ssh-keyscan -t rsa -H tm.pro-crafting.com >> ~/.ssh/known_hosts
 scp data@tm.pro-crafting.com:/home/data/$TRAVIS_BUILD_NUMBER/ ./
