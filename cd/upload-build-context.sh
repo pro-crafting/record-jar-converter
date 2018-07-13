9b33b992a77cbf2da92b9156a753e26e3df3c11c
Martin Panzer <postremus1996@googlemail.com>
2018-07-13 22:06:28 +0200

    fix copy destinations

--- cd/upload-build-context.sh
+++ cd/upload-build-context.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-cp -r record-jar-converter-rest-api/target cache/record-jar-converter-rest-api/target
-cp -r record-jar-converter-service/target cache/record-jar-converter-service/target
-cp -r record-jar-converter-web/target cache/record-jar-converter-web/target
+cp -r record-jar-converter-rest-api/target cache/record-jar-converter-rest-api
+cp -r record-jar-converter-service/target cache/record-jar-converter-service
+cp -r record-jar-converter-web/target cache/record-jar-converter-web
