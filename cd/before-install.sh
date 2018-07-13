2b61de7a5bc5387d4a8d9e6dd37269fc9cedfaf8
Martin Panzer <postremus1996@googlemail.com>
2018-07-13 21:42:44 +0200

    ls debug output

--- cd/before-install.sh
+++ cd/before-install.sh
@@ -5,15 +5,3 @@ mkdir cache
 #if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     gpg --fast-import cd/codesigning.asc
 #fi
-
-mkdir record-jar-converter-rest-api/target
-mkdir record-jar-converter-service/target
-mkdir record-jar-converter-web/target
-
-mkdir -p cache/record-jar-converter-rest-api/target
-mkdir -p cache/record-jar-converter-service/target
-mkdir -p cache/record-jar-converter-web/target
-
-cp -r cache/record-jar-converter-rest-api/target record-jar-converter-rest-api/target
-cp -r cache/record-jar-converter-service/target record-jar-converter-service/target
-cp -r cache/record-jar-converter-web/target record-jar-converter-web/target
---
+++ cd/download-build-context.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+mkdir record-jar-converter-rest-api/target
+mkdir record-jar-converter-service/target
+mkdir record-jar-converter-web/target
+
+mkdir -p cache/record-jar-converter-rest-api/target
+mkdir -p cache/record-jar-converter-service/target
+mkdir -p cache/record-jar-converter-web/target
+
+cp -r cache/record-jar-converter-rest-api/target record-jar-converter-rest-api/target
+cp -r cache/record-jar-converter-service/target record-jar-converter-service/target
+cp -r cache/record-jar-converter-web/target record-jar-converter-web/target
+
+
+
+ls record-jar-converter-rest-api/target
+ls record-jar-converter-service/target
+ls record-jar-converter-web/target
+
+ls cache/record-jar-converter-rest-api/target
+ls cache/record-jar-converter-service/target
+ls cache/record-jar-converter-web/target
