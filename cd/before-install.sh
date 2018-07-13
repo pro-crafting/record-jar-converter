c4968d3e685f9d3f5e4f8b3650467a3ba37acbdc
Martin Panzer <postremus1996@googlemail.com>
2018-07-13 21:26:08 +0200

    -p

--- cd/before-install.sh
+++ cd/before-install.sh
@@ -10,9 +10,9 @@ mkdir record-jar-converter-rest-api/target
 mkdir record-jar-converter-service/target
 mkdir record-jar-converter-web/target
 
-mkdir cache/record-jar-converter-rest-api/target
-mkdir cache/record-jar-converter-service/target
-mkdir cache/record-jar-converter-web/target
+mkdir -p cache/record-jar-converter-rest-api/target
+mkdir -p cache/record-jar-converter-service/target
+mkdir -p cache/record-jar-converter-web/target
 
 cp -r cache/record-jar-converter-rest-api/target record-jar-converter-rest-api/target
 cp -r cache/record-jar-converter-service/target record-jar-converter-service/target
