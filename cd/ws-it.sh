727853a73e2bc33699e074ae26fb79a66ee0d532
Martin Panzer <postremus1996@googlemail.com>
2018-07-14 13:19:56 +0200

    Stop can't be run from the command line

--- cd/ws-it.sh
+++ cd/ws-it.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 cd ./record-jar-converter-web/
-mvn wildfly-swarm:start failsafe:integration-test wildfly-swarm:stop failsafe:verify -P ws-it
+mvn wildfly-swarm:start failsafe:integration-test failsafe:verify -P ws-it
