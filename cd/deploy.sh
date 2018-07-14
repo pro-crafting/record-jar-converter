33daccb9bf6561d34df748ecca57af83a81ba383
Martin Panzer <postremus1996@googlemail.com>
2018-07-14 15:12:32 +0200

    bla

--- cd/deploy.sh
+++ cd/deploy.sh
@@ -2,7 +2,7 @@
 #if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     mvn -Dmaven.test.skip=true deploy -P sign,!docker-it,!ws-it
 
-    docker login --username $DOCKER_IO_USERNAME --pasword $DOCKER_IO_TOKEN
+    docker login --username $DOCKER_IO_USERNAME --password $DOCKER_IO_TOKEN
     docker load -i cache/images.tar
     docker push postremus/record-jar-converter-web
     docker push postremus/record-jar-converter-rest-api
