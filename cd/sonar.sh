9ad2ad00c9e966be24a2775bb97fe989ba67dbec
Martin Panzer <postremus1996@googlemail.com>
2018-07-14 21:43:20 +0200

    add sonar token to sonar execution

--- cd/sonar.sh
+++ cd/sonar.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install sonar:sonar -P !ws-it
+mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install sonar:sonar -P !ws-it -Dsonar.login=$SONAR_TOKEN
