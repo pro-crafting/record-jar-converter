deefae368d3a4ed1fab28c72a261b5cd5da55df6
Martin Panzer <postremus1996@googlemail.com>
2018-07-14 14:27:22 +0200

    maybe now? please?

--- cd/download-build-context.sh
+++ cd/download-build-context.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 
-echo "==== 1"
-ls record-jar-converter-rest-api/target
-ls record-jar-converter-service/target
-ls record-jar-converter-web/target
-echo "==== 2"
-ls -lR cache/
-echo "==== 3"
-
 mkdir record-jar-converter-rest-api/target
 mkdir record-jar-converter-service/target
 mkdir record-jar-converter-web/target
@@ -16,9 +8,9 @@ mkdir -p cache/record-jar-converter-rest-api/target
 mkdir -p cache/record-jar-converter-service/target
 mkdir -p cache/record-jar-converter-web/target
 
-cp -r cache/record-jar-converter-rest-api/target record-jar-converter-rest-api
-cp -r cache/record-jar-converter-service/target record-jar-converter-service
-cp -r cache/record-jar-converter-web/target record-jar-converter-web
+cp -r cache/record-jar-converter-rest-api/target/* record-jar-converter-rest-api/target/
+cp -r cache/record-jar-converter-service/target/* record-jar-converter-service/target/
+cp -r cache/record-jar-converter-web/target/* record-jar-converter-web/target/
 
 
 echo "==== 4"
--- cd/upload-build-context.sh
+++ cd/upload-build-context.sh
@@ -7,9 +7,9 @@ mkdir -p cache/record-jar-converter-rest-api/target
 mkdir -p cache/record-jar-converter-service/target
 mkdir -p cache/record-jar-converter-web/target
 
-cp -r record-jar-converter-rest-api/target cache/record-jar-converter-rest-api/target
-cp -r record-jar-converter-service/target cache/record-jar-converter-service/target
-cp -r record-jar-converter-web/target cache/record-jar-converter-web/target
+cp -r record-jar-converter-rest-api/target/* cache/record-jar-converter-rest-api/target
+cp -r record-jar-converter-service/target/* cache/record-jar-converter-service/target
+cp -r record-jar-converter-web/target/* cache/record-jar-converter-web/target
 
 docker save postremus/record-jar-converter-web postremus/record-jar-converter-rest-api -o cache/images.tar
 
