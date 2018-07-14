aa72c01a2868a5703dde1ab2afa7b4691724a1c3
Martin Panzer <postremus1996@googlemail.com>
2018-07-14 22:03:46 +0200

    set sonar.host option

--- cd/sonar.sh
+++ cd/sonar.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install sonar:sonar -P !ws-it -Dsonar.login=$SONAR_TOKEN
+mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install sonar:sonar -P !ws-it -Dsonar.login=$SONAR_TOKEN -Dsonar.host.url=https://sonarcloud.io
