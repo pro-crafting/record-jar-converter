8d72f58c9d19591f66f4d7506ba00638ecfdb482
Martin Panzer <postremus1996@googlemail.com>
2018-07-14 14:10:25 +0200

    mkdir ?

--- cd/upload-build-context.sh
+++ cd/upload-build-context.sh
@@ -3,6 +3,10 @@
 rm -rf cache
 mkdir cache
 
+mkdir -p cache/record-jar-converter-rest-api/target
+mkdir -p cache/record-jar-converter-service/target
+mkdir -p cache/record-jar-converter-web/target
+
 cp -r record-jar-converter-rest-api/target cache/record-jar-converter-rest-api/target
 cp -r record-jar-converter-service/target cache/record-jar-converter-service/target
 cp -r record-jar-converter-web/target cache/record-jar-converter-web/target
