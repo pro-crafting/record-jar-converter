bbe5ac5cc1911fb8f49a4e7dc03cdded9cfe19b7
Martin Panzer <postremus1996@googlemail.com>
2018-07-14 12:29:51 +0200

    run docker-it using the goals This resembles a real world environement a bit more

--- cd/docker-it.sh
+++ cd/docker-it.sh
@@ -2,4 +2,4 @@
 docker load -i cache/images.tar
 
 cd ./record-jar-converter-web/
-mvn verify -P docker,docker-it,!ws-it
+mvn docker:start failsafe:integration-test failsafe:verify docker:stop -P docker,docker-it,!ws-it
