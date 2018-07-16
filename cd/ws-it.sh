d68669be87ce38238f206d53cadc2d79226f31a8
Martin Panzer <postremus1996@googlemail.com>
2018-07-16 23:32:44 +0200

    update to thorntail 2.0.0.FINAL (#25)

--- cd/ws-it.sh
+++ cd/ws-it.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 cd ./record-jar-converter-web/
-mvn wildfly-swarm:start failsafe:integration-test failsafe:verify -P ws-it
+mvn thorntail:start failsafe:integration-test failsafe:verify -P ws-it
