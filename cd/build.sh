9d98451b88f7cce096ed134999b1d5e4971e7203
Martin Panzer <postremus1996@googlemail.com>
2019-04-07 12:09:19 +0200

    Build improvements (#34)
    * Build improvements
    
    Remove testing of wildfly swarm by itself. Integration Testing of the docker image on travis should be enough
    
    * Remove debug profile
    
    for debugging, just use a jar application configuration
    
    * fix travis trying to start ws-it

--- cd/build.sh
+++ cd/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    mvn deploy -P sign,docker,docker-it,!ws-it,build-extras
+    mvn deploy -P sign,docker,docker-it,build-extras
 else
-    mvn install -P docker,docker-it,!ws-it,build-extras
+    mvn install -P docker,docker-it,build-extras
 fi
