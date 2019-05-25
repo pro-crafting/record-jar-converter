a1eeb84f9eabf92455ea965a0880a49209314c24
Martin Panzer <postremus1996@googlemail.com>
2019-05-25 20:01:29 +0200

    try batch import so that gpg imports them without passphrase

--- cd/before-install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-cp cd/.travis.settings.xml $HOME/.m2/settings.xml
-openssl aes-256-cbc -K $encrypted_2789e35462ef_key -iv $encrypted_2789e35462ef_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
-mkdir cache
-if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    gpg --fast-import cd/codesigning.asc
-fi
