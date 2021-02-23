673d182d3fb4570246c03a3879f7fa3dc3d8eb0a
Martin Panzer <postremus1996@googlemail.com>
2021-02-23 21:44:29 +0100

    remove imports

--- src/test/java/com/pro_crafting/tools/recordjarconverter/StatusIT.java
+++ src/test/java/com/pro_crafting/tools/recordjarconverter/StatusIT.java
@@ -1,10 +1,6 @@
 package com.pro_crafting.tools.recordjarconverter;
 
-import io.restassured.RestAssured;
-import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.Test;
-import static io.restassured.RestAssured.*;
-import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 
 import static io.restassured.RestAssured.get;
